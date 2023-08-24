cfce5bb5613487e568304394d1b1b96ec3f5e6bb
Muhammad Musa <201370012@gift.edu.pk>
2023-08-24 08:23:05 -0700

    Fix

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>GIFT</Text>
       <Text>Muhammad Musa - 201370012</Text>
       <StatusBar style="auto" />
     </View>
